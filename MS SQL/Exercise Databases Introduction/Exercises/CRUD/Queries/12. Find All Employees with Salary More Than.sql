1c601e00519310f6131606e2cb451cd2d7f529d1
MustyX23 <mustyx900@gmail.com>
2023-08-31 21:46:35 +0300

    Create 12. Find All Employees with Salary More Than.sql

---
+++ MS SQL/Exercise Databases Introduction/Exercises/CRUD/Queries/12. Find All Employees with Salary More Than.sql
@@ -0,0 +1,7 @@
+--Create a SQL query that finds the first name, last name and salary for employees with a salary higher than 50000.
+--Order the result in decreasing order by salary.
+
+SELECT FirstName, LastName, Salary
+FROM Employees
+WHERE Salary > 50000
+ORDER BY Salary DESC
