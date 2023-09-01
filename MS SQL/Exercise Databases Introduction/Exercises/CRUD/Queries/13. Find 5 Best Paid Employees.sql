ee01c6daced25021c3900f5ac4439ed1209a099c
MustyX23 <mustyx900@gmail.com>
2023-09-01 12:43:54 +0300

    Create 13. Find 5 Best Paid Employees.sql

---
+++ MS SQL/Exercise Databases Introduction/Exercises/CRUD/Queries/13. Find 5 Best Paid Employees.sql
@@ -0,0 +1,4 @@
+--Create a SQL query that finds the first and last names of the 5 best-paid Employees, ordered descending by their salary.
+SELECT TOP 5 FirstName, LastName
+FROM Employees
+ORDER BY Salary DESC
