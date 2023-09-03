5cc7cabdbb439763db3b284ac0a67e908b72632e
MustyX23 <mustyx900@gmail.com>
2023-09-03 08:59:38 +0300

    Create 07. Employees With Project.sql

---
+++ MS SQL/Exercise Databases Introduction/Exercises/Subqueries and Joins/07. Employees With Project.sql
@@ -0,0 +1,7 @@
+SELECT TOP 5 e.EmployeeID, e.FirstName, p.Name AS [ProjectName]
+FROM Employees AS e
+	JOIN EmployeesProjects AS ep 
+		ON ep.EmployeeID = e.EmployeeID
+	JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13'
+ORDER BY EmployeeID
