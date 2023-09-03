bed7d379c39b36d0bfd261065aa1da43841ccc90
MustyX23 <mustyx900@gmail.com>
2023-09-03 08:59:35 +0300

    Create 05. Employees Without Projects.sql

---
+++ MS SQL/Exercise Databases Introduction/Exercises/Subqueries and Joins/05. Employees Without Projects.sql
@@ -0,0 +1,6 @@
+SELECT TOP 3 e.EmployeeID, e.FirstName 
+FROM Employees AS e
+FULL OUTER JOIN EmployeesProjects as ep
+	ON e.EmployeeID = ep.EmployeeID
+WHERE ep.EmployeeID IS NULL
+ORDER BY e.EmployeeID
