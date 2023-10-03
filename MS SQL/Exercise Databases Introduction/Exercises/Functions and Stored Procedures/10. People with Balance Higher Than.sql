24c46ffc7968b4c7c8505f9c4bb9804f80924933
MustyX23 <mustyx900@gmail.com>
2023-10-03 19:59:53 +0300

    Create 10. People with Balance Higher Than.sql

---
+++ MS SQL/Exercise Databases Introduction/Exercises/Functions and Stored Procedures/10. People with Balance Higher Than.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE usp_GetHoldersWithBalanceHigherThan (@Number MONEY)
+AS
+	SELECT [ah].[FirstName], [ah].[LastName]
+	FROM 
+	(
+		SELECT [AccountHolderId], SUM([Balance]) AS [TotalBalance] FROM [Accounts]
+		GROUP BY [AccountHolderId]
+	)
+	AS [data]
+JOIN [AccountHolders] AS [ah] ON [data].AccountHolderId = [ah].Id
+WHERE [data].TotalBalance > @Number
+ORDER BY [ah].FirstName, [ah].LastName
+
