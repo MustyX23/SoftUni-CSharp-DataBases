5e089f138d89b76afa36f690bcb8a1016fd6a455
MustyX23 <mustyx900@gmail.com>
2023-10-15 11:45:59 +0300

    Create 01. DDL.sql

---
+++ MS SQL/Exams/Databases MSSQL Server Regular Exam - 15 Oct 2023/01. DDL.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Countries (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL
+);
+CREATE TABLE Destinations (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    CountryId INT NOT NULL,
+    FOREIGN KEY (CountryId) REFERENCES Countries(Id)
+);
+CREATE TABLE Rooms (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Type NVARCHAR(40) NOT NULL,
+    Price DECIMAL(18, 2) NOT NULL,
+    BedCount INT NOT NULL CHECK (BedCount > 0 AND BedCount <= 10)
+);
+CREATE TABLE Hotels (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    DestinationId INT NOT NULL,
+    FOREIGN KEY (DestinationId) REFERENCES Destinations(Id)
+);
+CREATE TABLE Tourists (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(80) NOT NULL,
+    PhoneNumber NVARCHAR(20) NOT NULL,
+    Email NVARCHAR(80),
+    CountryId INT NOT NULL,
+    FOREIGN KEY (CountryId) REFERENCES Countries(Id)
+);
+CREATE TABLE Bookings (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    ArrivalDate DATETIME2 NOT NULL,
+    DepartureDate DATETIME2 NOT NULL,
+    AdultsCount INT NOT NULL CHECK (AdultsCount >= 1 AND AdultsCount <= 10),
+    ChildrenCount INT NOT NULL CHECK (ChildrenCount >= 0 AND ChildrenCount <= 9),
+    TouristId INT NOT NULL,
+    HotelId INT NOT NULL,
+    RoomId INT NOT NULL,
+    FOREIGN KEY (TouristId) REFERENCES Tourists(Id),
+    FOREIGN KEY (HotelId) REFERENCES Hotels(Id),
+    FOREIGN KEY (RoomId) REFERENCES Rooms(Id)
+);
+CREATE TABLE HotelsRooms (
+    HotelId INT NOT NULL,
+    RoomId INT NOT NULL,
+    PRIMARY KEY (HotelId, RoomId),
+    FOREIGN KEY (HotelId) REFERENCES Hotels(Id),
+    FOREIGN KEY (RoomId) REFERENCES Rooms(Id)
+);
