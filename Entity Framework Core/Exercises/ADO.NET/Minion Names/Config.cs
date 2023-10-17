a0b04091599d71bc439e0f3a65f7e4549b5cc11a
MustyX23 <mustyx900@gmail.com>
2023-10-17 22:37:42 +0300

    Create Config.cs

---
+++ Entity Framework Core/Exercises/ADO.NET/Minion Names/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minion_Names
+{
+    public static class Config
+    {
+        public const string ConnectionString = "Server=.;Database=MinionsDB;Trusted_Connection=true;TrustServerCertificate=true";
+    }
+}
