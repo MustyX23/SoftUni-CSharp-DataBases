3fc18642e96f1028a5d55448acd1a5ec15b5fcb0
MustyX23 <mustyx900@gmail.com>
2023-10-17 22:37:36 +0300

    Create Queries.cs

---
+++ Entity Framework Core/Exercises/ADO.NET/AddMinion/Queries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddMinion
+{
+    internal static class Queries
+    {
+        public const string GetVillainId = "SELECT Id FROM Villains WHERE Name = @Name";
+        public const string GetMinnionId = "SELECT Id FROM Minions WHERE Name = @Name";
+        public const string GetTownId = "SELECT Id FROM Towns WHERE Name = @townName";
+
+        public const string AddMinnionToVillain
+            = "INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (@minionId, @villainId)";
+        public const string AddNewTown = "INSERT INTO Towns (Name) VALUES (@townName)";
+        public const string AddNewVillain = "INSERT INTO Villains (Name, EvilnessFactorId)  VALUES (@villainName, 4)";
+        public const string AddNewMinnion = "INSERT INTO Minions (Name, Age, TownId) VALUES (@name, @age, @townId)";
+    }
+}
