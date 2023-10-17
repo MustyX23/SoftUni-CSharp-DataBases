e4c9d416d5da0a7d2fb5eb966bec5836d06871db
MustyX23 <mustyx900@gmail.com>
2023-10-17 22:37:39 +0300

    Create Program.cs

---
+++ Entity Framework Core/Exercises/ADO.NET/Initial Setup/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using Minion_Names;
+using System;
+
+namespace Initial_Setup
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(Config.ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount " +
+                               "FROM Villains AS v " +
+                               "JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                               "GROUP BY v.Id, v.Name " +
+                               "HAVING COUNT(mv.VillainId) > 3" +
+                               "ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0]} - {reader[1]}");
+                }
+                
+            }
+        }
+    }
+}
