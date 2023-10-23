18d2bf81dcbb06c227bf2f1657a1df363fc43b00
Mustafa Buhov <mustyx900@gmail.com>
2023-10-23 14:54:34 +0300

    --Employees with Salary Over 50 000

--- Entity Framework Core/Exercises/EntityFramework Introduction/DatabaseFirst/DatabaseFirst/StartUp.cs
+++ Entity Framework Core/Exercises/EntityFramework Introduction/DatabaseFirst/DatabaseFirst/StartUp.cs
@@ -10,7 +10,7 @@ namespace SoftUni
         {
             SoftUniContext context = new SoftUniContext();
 
-            string result = GetEmployeesFullInformation(context);
+            string result = GetEmployeesWithSalaryOver50000(context);
 
             Console.WriteLine(result);
         }
@@ -35,6 +35,29 @@ namespace SoftUni
             return sb.ToString().TrimEnd();
 
         }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context) 
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    FirstNameAndSalary = $"{e.FirstName} - {e.Salary:f2}"
+                })
+                .OrderBy(e => e.FirstName);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine(employee.FirstNameAndSalary);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
     }
 }
 
