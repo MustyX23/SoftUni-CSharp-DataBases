ef86945cad57d8e5de311105b7a7f1075514e031
Mustafa Buhov <mustyx900@gmail.com>
2023-10-23 14:47:51 +0300

    --Employees Full Information

--- Entity Framework Core/Exercises/EntityFramework Introduction/DatabaseFirst/DatabaseFirst/StartUp.cs
+++ Entity Framework Core/Exercises/EntityFramework Introduction/DatabaseFirst/DatabaseFirst/StartUp.cs
@@ -1,9 +1,38 @@
-﻿namespace SoftUni 
+﻿using SoftUni.Data;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni 
 {
     public class StartUp
     {
         static void Main(string[] args)
         {
+            SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.EmployeeId,
+                    Name = e.FirstName + " " + e.LastName + " " + e.MiddleName,
+                    JobInfo = e.JobTitle + " " + $"{e.Salary:f2}"
+                })
+                .OrderBy(e => e.EmployeeId);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine(employee.Name +" " + employee.JobInfo);
+            }
+            
+            return sb.ToString().TrimEnd();
 
         }
     }
