3647993596fa8bead695e9f9c34682971adf671b
Mustafa Buhov <mustyx900@gmail.com>
2023-10-23 14:34:10 +0300

    DatabaseFirst Method

---
+++ Entity Framework Core/Exercises/EntityFramework Introduction/EFCoreIntroductionDemo/EFCoreIntroductionDemo/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using EFCoreIntroductionDemo.Data;
+using EFCoreIntroductionDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        var db = new SoftUniContext();
+
+        var employees = db.Employees
+            .Where(e => e.Salary > 50000)
+            .Include(e => e.Address)
+            .Select(e => new
+            {
+                Name = e.FirstName + " " + e.LastName,
+                AddressEmployee = e.Address
+            });
+
+        foreach (var employee in employees)
+        {
+            Console.WriteLine(employee.Name + " " + employee.AddressEmployee);
+        }
+        
+
+
+    }
+}
