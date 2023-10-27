2b327fca806b75fe4f68050798e1ac0fb7741b4c
Mustafa Buhov <mustyx900@gmail.com>
2023-10-27 16:55:16 +0300

    -- SalesDatabase

---
+++ Entity Framework Core/Exercises/Entity Relations/Code First More Excercise/Hospital/P03_SalesDatabase/Data/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Sales = new HashSet<Sale>();
+        }
+        public int CustomerId { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Unicode(false)]
+        public string Email { get; set; }
+
+        public string CreditCardNumber {  get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+
+    }
+}
---
+++ Entity Framework Core/Exercises/Entity Relations/Code First More Excercise/Hospital/P03_SalesDatabase/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using P03_SalesDatabase.Data;
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        public static void Main() 
+        {
+            var context = new SalesContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
