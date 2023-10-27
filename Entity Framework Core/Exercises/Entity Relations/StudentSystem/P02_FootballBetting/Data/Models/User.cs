80498d26a428559db8d989dfa5f71dd9cd7e31b1
Mustafa Buhov <mustyx900@gmail.com>
2023-10-27 13:10:22 +0300

    Update User.cs

--- Entity Framework Core/Exercises/Entity Relations/StudentSystem/P02_FootballBetting/Data/Models/User.cs
+++ Entity Framework Core/Exercises/Entity Relations/StudentSystem/P02_FootballBetting/Data/Models/User.cs
@@ -9,16 +9,22 @@ namespace P02_FootballBetting.Data.Models
 {
     public class User
     {
+        public User()
+        {
+            Bets = new HashSet<Bet>();
+        }
         public int UserId { get; set; }
 
-        [Required]
         public string Username { get; set; }
 
-        [Required]
         public string Password { get; set; }
 
         public string Email { get; set; }
 
+        public string Name { get; set; }
+
         public decimal Balance {  get; set; }
+
+        public ICollection<Bet> Bets { get; set; }
     }
 }
