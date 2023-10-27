b255ca960200e0549ba8a8cd7800b29a6678174d
Mustafa Buhov <mustyx900@gmail.com>
2023-10-27 13:10:15 +0300

    Update Player.cs

--- Entity Framework Core/Exercises/Entity Relations/StudentSystem/P02_FootballBetting/Data/Models/Player.cs
+++ Entity Framework Core/Exercises/Entity Relations/StudentSystem/P02_FootballBetting/Data/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,27 @@ namespace P02_FootballBetting.Data.Models
 {
     public class Player
     {
+        public Player()
+        {
+            PlayersStatistics = new HashSet<PlayerStatistic>();
+        }
+
         public int PlayerId { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; }= null!;
 
-        public string SquadNumber { get; set; }
+        public int SquadNumber { get; set; }
 
-        public int TeamId {  get; set; }
+        public int TeamId { get; set; }
+
+        public Team Team { get; set; }
 
         public int PositionId { get; set; }
 
+        public Position Position { get; set; }
+
         public bool IsInjured {  get; set; }
+
+        public ICollection<PlayerStatistic> PlayersStatistics { get; set; }
     }
 }
