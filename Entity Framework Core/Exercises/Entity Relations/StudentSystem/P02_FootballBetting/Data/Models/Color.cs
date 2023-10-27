9465b26fdce369b7762bcefcbdcb8f4e81f87daf
Mustafa Buhov <mustyx900@gmail.com>
2023-10-27 13:10:12 +0300

    Update Color.cs

--- Entity Framework Core/Exercises/Entity Relations/StudentSystem/P02_FootballBetting/Data/Models/Color.cs
+++ Entity Framework Core/Exercises/Entity Relations/StudentSystem/P02_FootballBetting/Data/Models/Color.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,16 @@ namespace P02_FootballBetting.Data.Models
 {
     public class Color
     {
+        public Color()
+        {
+            PrimaryKitTeams = new HashSet<Team>();
+            SecondaryKitTeams = new HashSet<Team>();
+        }
         public int ColorId {  get; set; }
         public string Name { get; set; }
+
+        public ICollection<Team> PrimaryKitTeams { get; set; }
+
+        public ICollection<Team> SecondaryKitTeams { get; set; }
     }
 }
