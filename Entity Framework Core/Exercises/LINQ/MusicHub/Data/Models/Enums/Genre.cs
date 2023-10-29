f7501aad19e0e0de5679f1323306f65ac48f02f6
Mustafa Buhov <mustyx900@gmail.com>
2023-10-29 15:17:01 +0200

    Create Genre.cs

---
+++ Entity Framework Core/Exercises/LINQ/MusicHub/Data/Models/Enums/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models.Enums
+{
+    public enum Genre
+    {
+        Blues = 10,
+        Rap = 20,
+        PopMusic = 30,
+        Rock = 40,
+        Jazz = 50
+    }
+}
