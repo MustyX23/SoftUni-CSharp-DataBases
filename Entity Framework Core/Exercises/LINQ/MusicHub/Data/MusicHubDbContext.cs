025cb1bebb5e1fbd56514d68293dbf314bdf91c8
Mustafa Buhov <mustyx900@gmail.com>
2023-10-29 15:17:09 +0200

    Create MusicHubDbContext.cs

---
+++ Entity Framework Core/Exercises/LINQ/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data
+{
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        //Add DbSets
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Performer> Performers { get; set; }
+
+        public DbSet<Producer> Producers { get; set; }
+
+        public DbSet<Writer> Writers { get; set; }
+
+        public DbSet<SongPerformer> SongsPerformers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.ConnectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Add PrimaryKeys to the mapping table
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SongPerformer>(entity =>
+            entity.HasKey(sp => new {sp.SongId, sp.PerformerId}));
+        }
+    }
+}
