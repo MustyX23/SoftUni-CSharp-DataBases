ed6ca24e7c4ee0fe5f81ee19f02939971313c49d
Mustafa Buhov <mustyx900@gmail.com>
2023-10-29 15:16:58 +0200

    Create Config.cs

---
+++ Entity Framework Core/Exercises/LINQ/MusicHub/Data/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data
+{
+    public static class Config
+    {
+        public const string ConnectionString = "Server=.;Database=MusicHub;" +
+                    "Trusted_Connection=true;TrustServerCertificate=true";
+    }
+}
