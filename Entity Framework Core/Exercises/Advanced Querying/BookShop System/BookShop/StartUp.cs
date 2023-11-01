2a8f2e2cf9dee4931602e38fbffe264039b09558
Mustafa Buhov <mustyx900@gmail.com>
2023-11-01 19:20:58 +0200

    Update StartUp.cs

--- Entity Framework Core/Exercises/Advanced Querying/BookShop System/BookShop/StartUp.cs
+++ Entity Framework Core/Exercises/Advanced Querying/BookShop System/BookShop/StartUp.cs
@@ -1,45 +1,72 @@
 ﻿namespace BookShop
 {
-    using BookShop.Models;
     using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using System;
     using System.Text;
+    using System.Linq;
 
     public class StartUp
     {
         public static void Main()
         {
-            using var db = new BookShopContext();
+            var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
 
-            string command = Console.ReadLine();
-
-            string result = GetBooksByAgeRestriction(db, command);
-            /*
-             Read input from the console in your main method 
-             and call your method with the necessary arguments.
-             Print the returned string to the console.
-             Ignore the casing of the input.    
-             */
+            string result = GetBooksByPrice(db);
 
             Console.WriteLine(result);
         }
-
-        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        public static string GetGoldenBooks(BookShopContext context)
         {
             StringBuilder sb = new StringBuilder();
 
-            var ageRestriction = Enum.Parse<AgeRestriction>(command, true);
-            var bookTitles = context.Books
-                .Where(b => b.AgeRestriction == ageRestriction)
-                .Select(b => b.Title)
-                .OrderBy(b => b)
-                .ToList();
+            var goldenEdition = Enum.Parse<EditionType>("Gold", true);
 
-            foreach (var bookTitle in bookTitles)
+            var books = context.Books
+                .Where(b => b.EditionType == goldenEdition && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .ToArray();
+
+            /*
+             * Return in a single string the titles of 
+             * the golden edition books that have less than 5000 copies
+             * , each on a new line. Order them by BookId ascending.
+             */
+
+            foreach (var bookTitle in books)
             {
-                sb.AppendLine(bookTitle);
+                sb.AppendLine(bookTitle.Title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new
+                {
+                    Title = b.Title,
+                    Price = b.Price
+                })
+                .OrderByDescending(b => b.Price)
+                .ToArray();
+
+            /*
+             Return in a single string all titles 
+             and prices of books with a price higher than 40, each 
+             on a new row in the format given below. 
+             Order them by price descending.
+             */
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - ${book.Price:f2}");
             }
 
             return sb.ToString().TrimEnd();
