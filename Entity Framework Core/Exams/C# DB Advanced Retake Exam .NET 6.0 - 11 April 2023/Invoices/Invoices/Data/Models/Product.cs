562359989182cce6ab7cc797505de44ccaca3708
MustyX23 <mustyx900@gmail.com>
2023-11-04 22:36:02 +0200

    Create Product.cs

---
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/Data/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using Invoices.Data.Models.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Invoices.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            ProductsClients = new HashSet<ProductClient>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        public virtual ICollection<ProductClient> ProductsClients { get; set; }
+
+    }
+}
