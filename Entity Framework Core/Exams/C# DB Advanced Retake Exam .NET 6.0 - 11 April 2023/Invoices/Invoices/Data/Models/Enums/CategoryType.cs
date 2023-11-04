1d478ebc2e2f2b57b20bf3ba8296af0494b7b407
MustyX23 <mustyx900@gmail.com>
2023-11-04 22:36:08 +0200

    Create CategoryType.cs

---
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/Data/Models/Enums/CategoryType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices.Data.Models.Enums
+{
+    public enum CategoryType
+    {
+        ADR = 10,
+        Filters = 20,
+        Lights = 30,
+        Others = 40, 
+        Tyres = 50
+    }
+}
