7de12eadd452635930fae1d4a36b18f53635bf02
Mustafa Buhov <mustyx900@gmail.com>
2023-11-08 10:31:41 +0200

    Create StartUp.cs

---
+++ Entity Framework Core/Exercises/JSON/ProductShop/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Text.Json;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new ProductShopContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            string inputJSON = File.ReadAllText(@"../../../Datasets/users.json");
+            Console.WriteLine(ImportUsers(db, inputJSON));
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count()}";
+        }
+    }
+}
