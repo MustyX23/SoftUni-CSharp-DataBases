68bc1c91b2955fc58dabdac8e71de6f47759746a
Mustafa Buhov <mustyx900@gmail.com>
2023-11-10 11:55:57 +0200

    Create CarDTO.cs

---
+++ Entity Framework Core/Exercises/JSON/Car Dealer/CarDealer/DTOs/Export/CarDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTOs.Export
+{
+    public class CarDTO
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TraveledDistance { get; set; }
+
+        [JsonIgnore]
+        public List<int> PartsId { get; set; }
+    }
+}
