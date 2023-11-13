a8c8f37bbd4607f6e91deec69d18028c724413ea
Mustafa Buhov <mustyx900@gmail.com>
2023-11-13 17:24:28 +0200

    Update CarDealerProfile.cs

--- Entity Framework Core/Exercises/XML/Car Dealer/CarDealer/CarDealerProfile.cs
+++ Entity Framework Core/Exercises/XML/Car Dealer/CarDealer/CarDealerProfile.cs
@@ -25,6 +25,23 @@ namespace CarDealer
 
 
             CreateMap<Car, ExportCarDTO>();
+            CreateMap<Car, ExportBMWCarDTO>()
+                .ForMember(s => s.CarId, opt => opt.MapFrom(s => s.Id));
+
+            CreateMap<Supplier, ExportLocalSuppliersDTO>();
+
+            CreateMap<Car, ExportCarWithListOfPartsDTO>()
+                .ForMember(d => d.Parts, opt => opt.MapFrom(s => s.PartsCars
+                    .Select(pc => pc.Part)
+                    .OrderByDescending(p => p.Price)
+                    .ToArray()));
+
+            CreateMap<Part, ExportListOfPartsDTO>();
+
+            this.CreateMap<Customer, ExportCustmerDTO>()
+                .ForMember(dest => dest.BoughtCars, mo => mo.MapFrom(s => s.Sales.Count))
+                .ForMember(dest => dest.SpentMoney,
+                mo => mo.MapFrom(s => s.Sales.SelectMany(x => x.Car.PartsCars).Sum(x => x.Part.Price)))
 
         }
     }
