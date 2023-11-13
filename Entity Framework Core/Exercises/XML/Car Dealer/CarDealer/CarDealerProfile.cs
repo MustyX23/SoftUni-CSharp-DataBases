ff7cec7c49b3acdaf98f7b0bf6e364522d99e6a0
Mustafa Buhov <mustyx900@gmail.com>
2023-11-13 13:19:32 +0200

    Create CarDealerProfile.cs

---
+++ Entity Framework Core/Exercises/XML/Car Dealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<ImportSupplierDTO, Supplier>();
+
+            CreateMap<ImportPartsDTO, Part>()
+                .ForMember(d => d.SupplierId, opt => opt.MapFrom(s => s.SupplierId.Value));
+
+            CreateMap<ImportCarDTO, Car>()
+                .ForSourceMember(d => d.Parts, opt => opt.DoNotValidate());
+
+            CreateMap<ImportCustomerDTO, Customer>();
+
+            CreateMap<ImportSaleDTO, Sale>()
+                .ForMember(d => d.CarId,
+                    opt => opt.MapFrom(s => s.CarId.Value));
+
+
+            CreateMap<Car, ExportCarDTO>();
+
+        }
+    }
+}
