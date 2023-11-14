a94605e8039183bd58062a2d411a20eca9876687
MustyX23 <mustyx900@gmail.com>
2023-11-14 11:12:20 +0200

    Create ImportClientDTO.cs

---
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/DataProcessor/ImportDto/ImportClientDTO.cs
@@ -0,0 +1,22 @@
+﻿using Invoices.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    [XmlType("Client")]
+    public class ImportClientDTO
+    {
+        public string Name { get; set; }
+
+        public string NumberVat { get; set; }
+
+        public ImportClientAddressDTO[] Addresses { get; set; }
+
+    }
+}
