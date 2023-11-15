bc3cb1c59403743ab824fb7cce0a2b50c84046f8
Mustafa Buhov <mustyx900@gmail.com>
2023-11-15 21:34:34 +0200

    Update ImportClientAddressDTO.cs

--- Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/DataProcessor/ImportDto/ImportClientAddressDTO.cs
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/DataProcessor/ImportDto/ImportClientAddressDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
@@ -11,14 +12,25 @@ namespace Invoices.DataProcessor.ImportDto
     [XmlType("Address")]
     public class ImportClientAddressDTO
     {
+        [Required]
+        [MaxLength(20)]
+        [MinLength(10)]
         public string StreetName { get; set; }
 
+        [Required]
         public int StreetNumber { get; set; }
 
+        [Required]
         public string PostCode { get; set; }
 
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
         public string City { get; set; }
 
+        [Required]
+        [MaxLength(15)]
+        [MinLength(5)]
         public string Country { get; set; }
 
     }
