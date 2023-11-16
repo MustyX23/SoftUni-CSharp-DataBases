6dc9ca2b2ebddd2c7cad0d34f47fe8551f874491
Mustafa Buhov <mustyx900@gmail.com>
2023-11-16 18:53:43 +0200

    Update Serializer.cs

--- Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/DataProcessor/Serializer.cs
@@ -1,18 +1,60 @@
 ﻿namespace Invoices.DataProcessor
 {
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
     using Invoices.Data;
+    using Invoices.Data.Models;
+    using Invoices.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System.Linq;
 
     public class Serializer
     {
         public static string ExportClientsWithTheirInvoices(InvoicesContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            IMapper mapper = InitializeMapper();
+
+            ClientExportDto[] clients = context.Clients
+                .Where(c => c.Invoices.Any(i => i.IssueDate >= date))
+                .ProjectTo<ClientExportDto>(mapper.ConfigurationProvider)
+                .OrderByDescending(c => c.InvoicesCount)
+                .ThenBy(c => c.Name)
+                .ToArray();
+
+            return xmlHelper.Serialize<ClientExportDto[]>(clients, "Clients");
         }
 
         public static string ExportProductsWithMostClients(InvoicesContext context, int nameLength)
         {
+            var products = context.Products
+                .Where(p => p.ProductsClients.Any(pc => pc.Client.Name.Length == nameLength))
+                .Select(p => new
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    Category = p.CategoryType.ToString(),
+                    Clients = p.ProductsClients.Select(pc => new
+                    {
+                        Name = pc.Client.Name,
+                        NumberVat = pc.Client.NumberVat
+                    })
+                    .OrderBy(p => p.Name)
+                    .ToArray()
+                })
+                .OrderByDescending(p => p.Clients.Count())
+                .ThenBy(p => p.Name)
+                .ToArray();
 
-            throw new NotImplementedException();
+            return JsonConvert.SerializeObject(products, Formatting.Indented);
+        }
+        private static IMapper InitializeMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<InvoicesProfile>();
+            }));
         }
     }
 }
