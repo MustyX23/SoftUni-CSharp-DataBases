bfcbfb6a0b9ba2175a13d9f49d64665e9df90e8e
Mustafa Buhov <mustyx900@gmail.com>
2023-11-16 18:53:40 +0200

    Update ImportInvoiceDTO.cs

--- Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/DataProcessor/ImportDto/ImportInvoiceDTO.cs
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam .NET 6.0 - 11 April 2023/Invoices/Invoices/DataProcessor/ImportDto/ImportInvoiceDTO.cs
@@ -11,6 +11,7 @@ namespace Invoices.DataProcessor.ImportDto
     public class ImportInvoiceDTO
     {
         [Required]
+        [Range(1000000000, 1500000000)]
         public int Number { get; set; }
 
         [Required]
@@ -23,6 +24,7 @@ namespace Invoices.DataProcessor.ImportDto
         public decimal Amount { get; set; }
 
         [Required]
+        [Range(0, 2)]
         public CurrencyType CurrencyType { get; set; }
 
         [Required]
