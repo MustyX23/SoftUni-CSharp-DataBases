0acbc8c83962fc8da45e47c7a110536a1b239d2b
Mustafa Buhov <mustyx900@gmail.com>
2023-11-18 20:05:31 +0200

    Create ExportClientDTO.cs

---
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/ExportDto/ExportClientDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.DataProcessor.ImportDto;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    public class ExportClientDTO
+    {
+        public string? Name {  get; set; }
+
+        public ExportClientTruckDto[] Trucks { get; set; } = null!;
+    }
+}
