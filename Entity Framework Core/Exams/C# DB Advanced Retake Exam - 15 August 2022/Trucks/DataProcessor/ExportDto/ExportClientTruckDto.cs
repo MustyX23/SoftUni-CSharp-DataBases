87625a22ab10ae5fd31680be93ced4503c223c6f
Mustafa Buhov <mustyx900@gmail.com>
2023-11-18 20:05:32 +0200

    Create ExportClientTruckDto.cs

---
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam - 15 August 2022/Trucks/DataProcessor/ExportDto/ExportClientTruckDto.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    public class ExportClientTruckDto
+    {
+        [JsonProperty("TruckRegistrationNumber")]
+        public string? RegistrationNumber { get; set; }
+
+        public string? VinNumber {  get; set; }
+
+        public int TankCapacity {  get; set; }
+
+        public int CargoCapacity { get; set; }
+
+        public string? CategoryType {  get; set; }
+
+        public string? MakeType { get; set; }
+    }
+}
