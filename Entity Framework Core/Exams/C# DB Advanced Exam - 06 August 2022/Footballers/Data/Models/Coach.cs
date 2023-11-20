e77594e06ce4484c228fab014e8a31dab3d18a81
Mustafa Buhov <mustyx900@gmail.com>
2023-11-20 14:48:03 +0200

    Create Coach.cs

---
+++ Entity Framework Core/Exams/C# DB Advanced Exam - 06 August 2022/Footballers/Data/Models/Coach.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Footballers.Data.Models
+{
+    public class Coach
+    {
+        public Coach()
+        {
+            Footballers = new HashSet<Footballer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Nationality { get; set; } = null!;
+
+        public ICollection<Footballer> Footballers { get; set; } = null!;
+
+    }
+}
