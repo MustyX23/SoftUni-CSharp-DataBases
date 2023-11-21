5792344144d0b9a001277d6e3debfd7fd723ed8b
Mustafa Buhov <mustyx900@gmail.com>
2023-11-21 13:54:40 +0200

    Skeleton Created

---
+++ Entity Framework Core/Exams/C# DB Advanced Exam - 01 April 2023/Boardgames/Data/Models/BoardgameSeller.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.Data.Models
+{
+    public class BoardgameSeller
+    {
+        [Required]
+        public int BoardgameId {  get; set; }
+
+        public Boardgame Boardgame { get; set; } = null!;
+
+        [Required]
+        public int SellerId {  get; set; }
+        public Seller Seller { get; set;} = null!;
+
+    }
+}
