ec1da65f2d5b9e91453feb0175bf2a1add2a570f
Mustafa Buhov <mustyx900@gmail.com>
2023-11-22 20:37:14 +0200

    Models Created

---
+++ Entity Framework Core/Exams/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+            Guns = new HashSet<Gun>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(40)]
+        public string ManufacturerName { get; set; } = null!;
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(100)]
+        public string Founded { get; set; } = null!;
+
+        public ICollection<Gun> Guns { get; set; } = null!;
+    }
+}
