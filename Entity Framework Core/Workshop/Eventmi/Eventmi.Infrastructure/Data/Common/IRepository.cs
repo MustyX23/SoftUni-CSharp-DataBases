d065f8b7f9bf359c53d170cadd0b3e4f6a1cec73
MustyX23 <mustyx900@gmail.com>
2023-11-27 19:18:13 +0200

    Repo Pattern

---
+++ Entity Framework Core/Workshop/Eventmi/Eventmi.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using Eventmi.Infrastructure.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventmi.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        Task AddAsync<T>(T entity) where T : class;        
+
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+
+        IQueryable<T> AllWithDeleted<T>() where T : class, IDeletable;
+
+        IQueryable<T> AllWithDeletedReadOnly<T>() where T : class, IDeletable;
+
+        void Delete<T>(T entity) where T : class, IDeletable;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Entity Framework Core/Workshop/Eventmi/Eventmi.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Eventmi.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        protected readonly EventmiDbContext context;
+
+        public Repository(EventmiDbContext context)
+        {
+            this.context = context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class => context.Set<T>();
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();         
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+
+        IQueryable<T> IRepository.AllWithDeleted<T>()
+        {
+            return DbSet<T>()
+                .IgnoreQueryFilters();
+        }      
+
+        IQueryable<T> IRepository.AllWithDeletedReadOnly<T>()
+        {
+            return DbSet<T>()
+                .IgnoreQueryFilters()
+                .AsNoTracking();
+        }
+
+        void IRepository.Delete<T>(T entity)
+        {
+            entity.IsActive = false;
+            entity.DeletedOn = DateTime.Now;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+
+    }
+}
