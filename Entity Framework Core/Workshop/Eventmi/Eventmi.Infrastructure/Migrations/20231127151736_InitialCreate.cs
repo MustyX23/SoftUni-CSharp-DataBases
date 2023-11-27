13a626f8ed7ab7fe6eee56723790d8bf251f058d
MustyX23 <mustyx900@gmail.com>
2023-11-27 19:20:34 +0200

    Migration Status

---
+++ Entity Framework Core/Workshop/Eventmi/Eventmi.Infrastructure/Migrations/20231127151736_InitialCreate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eventmi.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Town",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Идентификатор на град")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false, comment: "Име на град")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Town", x => x.Id);
+                },
+                comment: "Град");
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Идентификатор на адреса")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Street = table.Column<string>(type: "nvarchar(85)", maxLength: 85, nullable: false, comment: "Улица"),
+                    TownId = table.Column<int>(type: "int", nullable: false, comment: "Идентификатор на град")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Address_Town_TownId",
+                        column: x => x.TownId,
+                        principalTable: "Town",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                },
+                comment: "Място на провеждане");
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Идентификатор на събитието")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "Име на събитието"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, comment: "Активност на събитието"),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Начало на събитието"),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Край на събитието"),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true, comment: "Дата на изтриване"),
+                    PlaceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Event_Address_PlaceId",
+                        column: x => x.PlaceId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                },
+                comment: "Събитие");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_TownId",
+                table: "Address",
+                column: "TownId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_PlaceId",
+                table: "Event",
+                column: "PlaceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Town");
+        }
+    }
+}
---
+++ Entity Framework Core/Workshop/Eventmi/Eventmi.Infrastructure/Migrations/20231127162744_SeedTowns.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eventmi.Infrastructure.Migrations
+{
+    public partial class SeedTowns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Town",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "София" },
+                    { 2, "Пловдив" },
+                    { 3, "Варна" },
+                    { 4, "Бургас" },
+                    { 5, "Велинград" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Town",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Town",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Town",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Town",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Town",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
