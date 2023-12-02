d1aec90cf46cfd5b6787a1a273cb1395e9b7be9c
Mustafa Buhov <mustyx900@gmail.com>
2023-12-02 19:32:44 +0200

    Import DTOS

---
+++ Entity Framework Core/Exams/C# DB Advanced Regular Exam - 02 December 2023/Medicines/DataProcessor/ImportDtos/ImportPatientDto.cs
@@ -0,0 +1,33 @@
+﻿using Medicines.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicines.DataProcessor.ImportDtos
+{
+    public class ImportPatientDto
+    {
+        public ImportPatientDto()
+        {
+            Medicines = new List<int>();    
+        }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(100)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [Range(0, 2)]
+        public int AgeGroup { get; set; }
+
+        [Required]
+        [Range(0, 1)]
+        public int Gender { get; set; }
+
+        public ICollection<int> Medicines { get; set; } = null!;
+    }
+}
